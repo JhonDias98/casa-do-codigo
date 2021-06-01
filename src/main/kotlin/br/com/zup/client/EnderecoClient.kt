03741001eda6876a8cdb421ea1d8bee40cdcb8d6
Jonathan Pereira Dias <52224050+JhonDias98@users.noreply.github.com>
2021-06-01 01:01:56 -0300

    Consumir endpoint rest em XML

--- src/main/kotlin/br/com/zup/client/EnderecoClient.kt
+++ src/main/kotlin/br/com/zup/client/EnderecoClient.kt
@@ -2,6 +2,7 @@ package br.com.zup.client
 
 import br.com.zup.endereco.EnderecoResponse
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.annotation.Client
 
@@ -9,4 +10,17 @@ import io.micronaut.http.client.annotation.Client
 interface EnderecoClient {
     @Get("{cep}/json")
     fun consultarEndereco(cep: String) : HttpResponse<EnderecoResponse>
+
+    /*
+    GET:
+    @Get(consumes = [MediaType.APPLICATION_XML])
+    @Consumes(MediaType.APPLICATION_XML)
+    POST:
+    @Post(produces = [MediaType.APPLICATION_XML])
+    @Produces(MediaType.APPLICATION_XML)
+     */
+
+    @Get("{cep}/xml",consumes = [MediaType.APPLICATION_XML])
+    fun consultarEnderecoXml(cep: String) : HttpResponse<EnderecoResponse>
+
 }
